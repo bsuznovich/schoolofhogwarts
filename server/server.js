7fd0da736e232edf6e39786276699b12b72d4303
Brody Suznovich <brody@Brodys-MacBook-Pro.local>
2019-01-14 17:56:52 -0700

    fixed bug

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const massive = require('massive')
+const express = require('express')
+// const controller = require('./controller')
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+const app = express()
+
+app.use(express.json())
+
+massive(CONNECTION_STRING).then((db) => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => {
+        console.log(`Revelio on port ${SERVER_PORT}`)
+    })
+})
