db41b5855d75c0c5a6483b3e94f795429f010003
Brody Suznovich <brody@Brodys-MBP.home>
2019-01-27 15:44:55 -0700

    css

--- src/Components/Houses/Houses.js
+++ src/Components/Houses/Houses.js
@@ -95,25 +95,25 @@ class Houses extends Component{
                             </div>
                                 <div className='descript'>
                                     <div className='ravendes'>
-                                    Ravenclaw is one of the four Houses of Hogwarts School of Witchcraft and Wizardry. Its founder was a medieval witch Rowena Ravenclaw. Members of this house are characterized by their wit, learning, and wisdom. The emblematic animal is symbol is an eagle, and blue and bronze are its colours.
+                                    Founded by a medieval witch named Rowena Ravenclaw. Members of this house are characterized by their wit, learning, and wisdom. The emblematic animal is symbol is an eagle, and blue and bronze are its colours.
                                     <br/>
                                     <br/>
                                     Ravenclaw corresponds roughly to the element of air, and it is for that reason that the House colours were chosen; blue and bronze represent the sky and eagle feathers respectively, both having much to do with air. The Ravenclaw points hourglass contains blue sapphires.
                                     </div>
                                     <div className='gryffindes'>
-                                    Gryffindor is one of the four Houses of Hogwarts School of Witchcraft and Wizardry and was founded by Godric Gryffindor. Godric instructed the Sorting Hat to choose students possessing characteristics he most valued, such as courage, chivalry, and determination, to be Sorted into his house. The emblematic animal is a lion, and its colours are scarlet and gold.
+                                    Founded by Godric Gryffindor. Godric instructed the Sorting Hat to choose students possessing characteristics he most valued, such as courage, chivalry, and determination, to be Sorted into his house. The emblematic animal is a lion, and its colours are scarlet and gold.
                                     <br/>
                                     <br/>
                                     Gryffindor corresponds roughly to the element of fire, and it is for this reason that the colours scarlet and gold were chosen to represent the house. The colour of fire corresponds to that of a lion as well, with scarlet representing the mane and tail and gold representing the coat.
                                     </div>
                                     <div className='huffledes'>
-                                    Hufflepuff is one of the four Houses of Hogwarts School of Witchcraft and Wizardry. Its founder was the medieval witch Helga Hufflepuff. Hufflepuff is the most inclusive among the four houses; valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its members. The emblematic animal is a badger, and yellow and black are its colours.
+                                    Founded by the medieval witch Helga Hufflepuff. Hufflepuff is the most inclusive among the four houses; valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its members. The emblematic animal is a badger, and yellow and black are its colours.
                                     <br/>
                                     <br/>
                                     Hufflepuff corresponds roughly to the element of earth, and it is for that reason that the House colours were chosen: yellow represented wheat, while black was emblematic of soil. The Hufflepuff points hourglass contains yellow diamonds. Students sorted into Hufflepuff often demonstrate exceptional abilities in Herbology, owing to their correspondence to earth.
                                     </div>
                                     <div className='slytherdes'>
-                                        Slytherin is one of the four Houses at Hogwarts School of Witchcraft and Wizardry, founded by Salazar Slytherin. In establishing the house, Salazar instructed the Sorting Hat to pick students who had a few particular characteristics he most valued. Those characteristics include: cunning, resourcefulness, and ambition.
+                                    Founded by Salazar Slytherin. In establishing the house, Salazar instructed the Sorting Hat to pick students who had a few particular characteristics he most valued. Those characteristics include: cunning, resourcefulness, and ambition.
                                     <br/>
                                     <br/>
                                         The emblematic animal of the house is a snake and its colours are green and silver. Slytherin corresponds roughly with the element of water due to serpents being commonly associated with the sea and lochs in western European mythology, as well as serpents being physically fluid and flexible animals.
